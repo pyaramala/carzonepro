509b92b37977380488f819dc574c5c3dd7616c5d
yaramalapradeep <pradeepyaramala1624@gmail.com>
2022-08-10 14:44:07 -0700

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,about,services,contact
 app_name = 'pages_app'
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('services/', services, name='services'),
+    path('contact/', contact, name='contact')
+    
 ]
